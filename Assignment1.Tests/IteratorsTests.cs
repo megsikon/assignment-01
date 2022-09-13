9d8c75f7dd7c7056bd7a8cc4b26d59e5baa8fb02
Magnus Emil Gervig Svensson <mesv@itu.dk>
2022-09-13 13:34:04 +0200

    implemented iterators and iterators test

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -2,4 +2,35 @@ namespace Assignment1.Tests;
 
 public class IteratorsTests
 {
+ 
+    [Fact]
+    public void Flatten_when_given_stream_of_stream_of_T_should_return_stream_of_T() {
+        //arrange
+        var stream_of_streams = new List<List<int>>();
+        stream_of_streams.Add(new List<int>() {1,2,3});
+        stream_of_streams.Add(new List<int>() {4,5,6});
+
+        var expected = new List<int>(){1,2,3,4,5,6};
+        //act
+        var actual = Iterators.Flatten(stream_of_streams);
+
+        //assert
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void Filter_when_given_stream_of_numbers_should_return_evens() {
+        //arrange
+        var stream_of_numbers = new List<int>(){1,2,3,4,5};
+        Predicate<int> even = Even;
+        bool Even(int i) => i % 2 == 0;
+        
+        var expected = new List<int>(){2,4};
+        
+        //act
+        var actual = Iterators.Filter(stream_of_numbers, even);
+        
+        //assert
+        Assert.Equal(expected, actual);
+    }
 }
--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -2,7 +2,26 @@ namespace Assignment1;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) 
+    {
+        foreach (var item in items)
+        {
+            foreach (var i in item)
+            {
+                yield return i;
+            }
+        }
+    }
 
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
+    {
+        foreach (var item in items)
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        
+        }
+    }
 }
